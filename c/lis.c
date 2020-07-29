390878ebb19ad08ca3e430cc5d2fc9d7db94fd07
Lady Red <mcscope@gmail.com>
2020-07-28 19:05:17 -0700

    Updating lis to take arguments, have more complex build process

--- c/lis.c
+++ c/lis.c
@@ -1,8 +1,13 @@
+#import <dirent.h>
 #import <stdio.h>
 #import <stdlib.h>
-#import <dirent.h>
-#include <time.h>
-#include <sys/stat.h>
+#import <strings.h>
+#import <sys/stat.h>
+#import <time.h>
+#import <unistd.h>
+
+
+char *getcwd(char *buf, size_t size);
 
 // use these 
 // printf("\x1b[<color>m");
@@ -25,43 +30,91 @@ enum Color {Black = 30,
 #define DT_LNK          10
 #define DT_SOCK         12
 #define DT_WHT          14
+#define printcolor(c)   printf("\x1b[%dm", c)
+#define dprintf   printf
 
-void print_entry(struct dirent * entry) {
+void print_size(long long int size) {
+    long long giga = 1000000000;
+    long long mega =    1000000;
+    long long kilo =       1000;
+    if (size > giga) {
+        printcolor(Red);
+        printf("%lldG", size/giga);
+        printcolor(White);
+    }  else if (size > mega) {
+        printcolor(Yellow);
+        printf("%lldM", size/mega);
+        printcolor(White);
+    } else if (size > kilo) {
+        printcolor(Green);
+        printf("%lldK", size/kilo);
+        printcolor(White);
+    }
+    else {
+        printcolor(White);
+        printf("%lld", size);
+        printcolor(White);
+    }
+}
+
+void print_entry(char* path, struct dirent * entry) {
     switch (entry -> d_type) {
 
     case  DT_DIR:
-        printf("\x1b[%dm", Cyan);
+        printcolor(Cyan);
         break;
     case  DT_REG:
-        printf("\x1b[%dm", Yellow);
+        printcolor(Yellow);
         break;
     default:
-        printf("\x1b[%dm", White);
+        printcolor(White);
     }
     printf("%-30s", entry->d_name);
     struct stat buf;
-    if (stat(entry->d_name, &buf)==0) {
+    char* filepath[1000];
+    strcpy(filepath, path);
+    strcat(filepath, "/");
+    strcat(filepath, entry->d_name);
+    if (stat(filepath, &buf)==0) {
         printf("\t ModAge: %10ld", time(NULL)-buf.st_mtime);
-        printf("\t Mode: %o", buf.st_mode);
-        printf("\t Size: %lld", buf.st_size);
-        printf("\t Blocks: %lld", buf.st_blocks);
+        printf("\t Mode: %o \t", buf.st_mode);
+        print_size(buf.st_size);
+        printf("\t Blocks: %lld \n", buf.st_blocks);
+    } else {
+        printf(" <bad info> \n");
     }
 
-    printf("\n\x1b[%dm", Yellow);
+    printcolor(White);
     fflush(stdout);
-
 }
 
-int main() {
-    DIR * dir = opendir("/Users/ladyred/Code/Bradfield/csi/prep/c");
+int lsdir(char* path) {
+    if(path[0] != '/') {
+        char cwd[1000];
+        getcwd(cwd, 1000);	
+        strcat(cwd, "/");
+        strcat(cwd, path);
+        path = cwd;
+    }
+
+    dprintf("opening path: %s\n", path);
+    DIR * dir = opendir(path);
     struct dirent *entry;
     while((entry=readdir(dir))) {
-        print_entry(entry);
-        // printf("%s %d %d %d\n", entry->d_name, entry->d_reclen, entry->d_type, entry->d_namlen);
+        print_entry(path, entry);
     }
-    
-    // for (char buf [100]; fgets(buf, 100, dir)!=NULL; printf("%s", buf));
-    printf("hello_world!");
-    printf("\x1b[%dm", White);
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    printf("%d\n", argc);
+    if( argc==1) {
+	return lsdir("");
+    }
+    int ret = 0;
+    for(;--argc>=1; ) {
+        ret += lsdir(argv[argc]);
+    }  
+    return ret;
 
 }
